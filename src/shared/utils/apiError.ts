e373edd59e185f30b3d64c0a07870a933f5a03bb
Ankit Nayak <ankitnayak882@gmail.com>
2026-09-07 12:01:20 +0530

    feat: implement custom error classes for API error handling

---
+++ src/shared/utils/apiError.ts
@@ -0,0 +1,29 @@
+class ApiError extends Error {
+  constructor(
+    public statusCode: number,
+    message: string,
+    public isOperational = true,
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class NotFoundError extends ApiError {
+  constructor(message = "Resource Not Found") {
+    super(404, message);
+  }
+}
+
+export class BadRequestError extends ApiError {
+  constructor(message = "Bad Request") {
+    super(400, message);
+  }
+}
+
+export class UnauthorizedError extends ApiError {
+  constructor(message = "Unauthorized") {
+    super(401, message);
+  }
+}
